94a8b3cdad63f8e176258cc57e67abf4d99b1ed6
pvp23 <patrickvonplaten@mbp-de-patrick.fritz.box>
2016-12-19 04:12:58 +0100

    extended guy user interface

--- code/gui/GUIStartFrame.java
+++ code/gui/GUIStartFrame.java
@@ -7,7 +7,9 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.security.KeyStore.PrivateKeyEntry;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -24,13 +26,28 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-import user_interface.GUI;
+
+
+import core.Core;
+import exceptions.AlreadyUsedUsernameException;
+import parsers.ParseCouriers;
+import parsers.ParseCustomers;
+import parsers.ParseManagers;
+import parsers.ParseRestaurants;
+import users.Address;
+import users.Courier;
+import users.Customer;
+import users.Manager;
+import users.Restaurant;
+import users.User;
 
 public class GUIStartFrame {
 	
+	private static Core core = new Core();
 	private static GUIStartFrame instance;
-
 	private JFrame frame = new JFrame("My Foodora");
+	
+	private HashMap<String, String> passwordUserMap = new HashMap<>();
 	
 	JPanel welcome_panel = new JPanel();
 	JPanel welcome_button_panel = new JPanel();
@@ -43,7 +60,6 @@ public class GUIStartFrame {
 	JRadioButton radio_customer = new JRadioButton("Customer", true);
 	JRadioButton radio_courier = new JRadioButton("Courier");
 	JRadioButton radio_restaurant = new JRadioButton("Restaurant");
-	JRadioButton radio_manager = new JRadioButton("Manager");
 	ButtonGroup user_type_group = new ButtonGroup();
 	
 	// buttons 
@@ -54,6 +70,36 @@ public class GUIStartFrame {
 	JButton backToRegister_button = new JButton("GO BACK");
 	JButton createAccount_button = new JButton("CREATE ACCOUNT");
 	JButton goToLogIn_button = new JButton("GO TO LOG IN");
+	JButton register_button = new JButton("REGISTER");
+	
+	
+	// Textfields to register
+	JTextField surname_JTF = new JTextField(15);
+	JTextField xCoordinate_JTF = new JTextField(15);
+	JTextField yCoordinate_JTF = new JTextField(15);
+	JTextField phoneNum_JTF = new JTextField(15);
+	JTextField emailAddress_JTF = new JTextField(15);
+	JTextField username_JTF = new JTextField(15);
+	JTextField password_JTF = new JTextField(15);
+	JTextField passwordConf_JTF = new JTextField(15);
+	JTextField name_JTF = new JTextField(15);
+	
+	// String to register
+	String surname; 
+	int xCoordinate;
+	int yCoordinate;
+	String phoneNum;
+	String emailAddress;
+	String username;
+	String passwort;
+	String passwortConf;
+	String name;
+ 
+	
+	
+	
+	
+	
 	
 	
 
@@ -68,7 +114,6 @@ public class GUIStartFrame {
 	JPanel customer_specific_info = new JPanel();
 	JPanel courier_specific_info = new JPanel();
 	JPanel restaurant_specific_info = new JPanel();
-	JPanel manager_specific_info = new JPanel();
 	
 	/*********************************************************/
 	// HelpFunctions
@@ -76,6 +121,12 @@ public class GUIStartFrame {
 	private static GUIStartFrame getInstance() {
 		if(instance == null){
 			instance = new GUIStartFrame();
+			core.logIn("root"); // login with manager to add lists
+			core.setCustomerList(ParseCustomers.parseCustomers("src/txtFILES/customersList.txt"));
+			core.setCourierList(ParseCouriers.parseCouriers("src/txtFILES/courierList.txt"));
+			core.setManagerList(ParseManagers.parseManagers("src/txtFILES/managersList.txt"));
+			core.setRestaurantList(ParseRestaurants.parseRestaurants("src/txtFILES/restaurantList.txt"));
+			core.logOut();
 		}
 		return instance;
 	}
@@ -95,105 +146,44 @@ public class GUIStartFrame {
 	    ask_info.setText("PLEASE FILL OUT THE FOLLOWING INFOS");
 	    ask_info.setEditable(false);
 		
-		JTextField name = new JTextField(15);
-		name.setPreferredSize(new Dimension(150, 30));
-		name.setForeground(Color.BLUE);
-		name.setText("Insert your name");
+		
+		name_JTF.setPreferredSize(new Dimension(150, 30));
+		name_JTF.setForeground(Color.BLUE);
+		name_JTF.setText("Insert your name");
 		
 		user_specific_info.setBackground(Color.green);
 		
 	    user_global_info.add(ask_info, BorderLayout.NORTH);
-		user_global_info.add(name, BorderLayout.NORTH);
+		user_global_info.add(name_JTF, BorderLayout.NORTH);
 	    user_global_info.add(user_specific_info, BorderLayout.CENTER);
 	}
 	
-	private void fillAddressInfosPanel() {
-		
-		JTextField xCoordinate = new JTextField(15);
-		xCoordinate.setText("Insert xCoordinate");
-		JTextField yCoordinate = new JTextField(15);
-		yCoordinate.setText("Insert yCoordinate");
-		
-		address_panel.add(xCoordinate);
-		address_panel.add(yCoordinate);
+	private void fillAddressInfosPanel() {	
+		address_panel.add(xCoordinate_JTF);
+		address_panel.add(yCoordinate_JTF);
 	}
 	
 	public void fillCourierSpecificInfosPanel() {
 		courier_specific_info.setLayout(new BorderLayout());
-		courier_specific_info.setBorder(BorderFactory.createEmptyBorder(30, 80, 30, 80));
+		courier_specific_info.setBorder(BorderFactory.createEmptyBorder(50, 80, 50, 80));
 		courier_specific_info.setBackground(Color.green);
-		
-		JTextField surname = new JTextField(15);
-		surname.setText("Insert your surname");
-		JTextField phoneNum = new JTextField(15);
-		phoneNum.setText("Insert your phone number");
-		
-		courier_specific_info.add(surname,BorderLayout.NORTH);
-		courier_specific_info.add(phoneNum,BorderLayout.NORTH);
-		courier_specific_info.add(address_panel, BorderLayout.SOUTH);
-	}
-	
-	public void fillManagerSpecificInfosPanel() {
-		manager_specific_info.setLayout(new BorderLayout());
-		manager_specific_info.setBorder(BorderFactory.createEmptyBorder(30, 80, 30, 80));
-		manager_specific_info.setBackground(Color.green);
-		
-		JTextField surname = new JTextField(15);
-		surname.setText("Insert your surname");
-		JTextField phoneNum = new JTextField(15);
-		phoneNum.setText("Insert your phone number");
-		
-		manager_specific_info.add(surname,BorderLayout.NORTH);
-		manager_specific_info.add(phoneNum,BorderLayout.NORTH);
-		manager_specific_info.add(address_panel, BorderLayout.SOUTH);
 	}
 	
 	public void fillCustomerSpecificInfosPanel() {
 		customer_specific_info.setLayout(new BorderLayout());
-		customer_specific_info.setBorder(BorderFactory.createEmptyBorder(30, 80, 30, 80));
+		customer_specific_info.setBorder(BorderFactory.createEmptyBorder(50, 80, 50, 80));
 		customer_specific_info.setBackground(Color.green);
-		
-		JTextField surname = new JTextField(15);
-		surname.setText("Insert your surname");
-		JTextField phoneNum = new JTextField(15);
-		phoneNum.setText("Insert your phone number");
-		JTextField emailAddress = new JTextField(15);
-		phoneNum.setText("Insert your Email address");
-		
-		customer_specific_info.add(surname,BorderLayout.NORTH);
-		customer_specific_info.add(phoneNum,BorderLayout.NORTH);
-		customer_specific_info.add(emailAddress,BorderLayout.NORTH);
-		customer_specific_info.add(address_panel, BorderLayout.SOUTH);
 	}
 	
 	public void fillRestaurantSpecificInfosPanel() {
 		restaurant_specific_info.setLayout(new BorderLayout());
 		restaurant_specific_info.setBorder(BorderFactory.createEmptyBorder(30, 80, 30, 80));
 		restaurant_specific_info.setBackground(Color.green);
-		
-		restaurant_specific_info.add(address_panel, BorderLayout.SOUTH);
 	}
 	
 	public void fillLoginPanel() {
 		login_panel.setBackground(Color.green);
 		login_panel.setBorder(BorderFactory.createEmptyBorder(30, 150, 10, 150));
-		
-		JTextField username = new JTextField(20);
-		username.setPreferredSize(new Dimension(150, 30));
-		username.setForeground(Color.BLUE);
-		username.setText("Insert your username");
-		//TODO find a function that deletes the text when clicked on 
-
-		JTextField password = new JTextField(20);
-		password.setPreferredSize(new Dimension(150, 30));
-		password.setForeground(Color.BLUE);
-		password.setText("Insert your password");
-		
-		
-		login_panel.add(username, BorderLayout.NORTH);
-		login_panel.add(password, BorderLayout.CENTER);
-		
-		
 	}
 	
 	public void fillRegisterPanel() {
@@ -203,26 +193,15 @@ public class GUIStartFrame {
 		user_type_group.add(radio_restaurant);
 		user_type_group.add(radio_courier);
 		user_type_group.add(radio_customer);
-		user_type_group.add(radio_manager);
 		
 		JPanel user_type = new JPanel();
 		user_type.setBackground(Color.red);
 		user_type.add(radio_customer);
 		user_type.add(radio_courier);
 		user_type.add(radio_restaurant);
-		user_type.add(radio_manager);
 		
-		JTextField username = new JTextField(15);
-		username.setForeground(Color.BLUE);
-		username.setText("Insert a username");
-
-		JTextField password = new JTextField(15);
-		password.setForeground(Color.BLUE);
-		password.setText("Insert a password");
 		
 		register_panel_info.add(user_type, BorderLayout.NORTH);
-		register_panel_info.add(username, BorderLayout.NORTH);
-		register_panel_info.add(password, BorderLayout.CENTER);
 	}
 	
 	public void fillWelcomePanel() {
@@ -281,8 +260,12 @@ public class GUIStartFrame {
         fillRegisterPanel();
         fillLoginPanel();
         fillUserGlobalInfoPanel();
-        fillCourierSpecificInfosPanel();
         fillAddressInfosPanel();
+        
+        fillCourierSpecificInfosPanel();
+        fillCustomerSpecificInfosPanel();
+        fillRestaurantSpecificInfosPanel();
+        
         
         setCurrentPanel(welcome_panel);
         
@@ -294,6 +277,10 @@ public class GUIStartFrame {
     	goToRegister_button.addActionListener(new GoToRegisterButton());
     	backToRegister_button.addActionListener(new GoBackToRegisterButton());
     	createAccount_button.addActionListener(new WhatAccountType());
+    	register_button.addActionListener(new RegisterButton());
+    	logIn_button.addActionListener(new LoginButton());
+    	
+    	
     	
 	}
    
@@ -307,38 +294,121 @@ public class GUIStartFrame {
         });
     }
 	
+	private boolean checkIfPassWordIsEqual(String passwort, String passwortConf) {
+		if(passwort.equals(passwortConf)){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	private void goToLogInPanel(){
+		
+		login_panel.add(home_button, BorderLayout.SOUTH);
+		login_panel.add(logIn_button, BorderLayout.SOUTH);
+		
+		username_JTF.setPreferredSize(new Dimension(150, 30));
+		username_JTF.setForeground(Color.BLUE);
+		username_JTF.setText("Insert your username");
+		//TODO find a function that deletes the text when clicked on 
+
+		password_JTF.setPreferredSize(new Dimension(150, 30));
+		password_JTF.setForeground(Color.BLUE);
+		password_JTF.setText("Insert your password");
+		
+		login_panel.add(username_JTF, BorderLayout.NORTH);
+		login_panel.add(password_JTF, BorderLayout.CENTER);
+		
+		setCurrentPanel(login_panel);
+	}
+	
+	private void goToRegisterPanel(){
+		register_panel_info.add(home_button, BorderLayout.SOUTH);
+		register_panel_info.add(createAccount_button, BorderLayout.SOUTH);
+		
+		register_panel_info.add(username_JTF, BorderLayout.NORTH);
+		register_panel_info.add(password_JTF, BorderLayout.CENTER);
+		register_panel_info.add(passwordConf_JTF, BorderLayout.CENTER);
+		
+		username_JTF.setForeground(Color.BLUE);
+		username_JTF.setText("Insert a username");
+
+		password_JTF.setForeground(Color.BLUE);
+		password_JTF.setText("Insert a password");
+		
+		passwordConf_JTF.setForeground(Color.BLUE);
+		passwordConf_JTF.setText("Confirm the password");
+		
+		setCurrentPanel(register_panel_info);
+	}
 	/*********************************************************/
 	/* ActionListeners */
 	
 	private class WhatAccountType implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			user_specific_info.removeAll();
-			if (radio_customer.isSelected()){
-				user_specific_info.add(customer_specific_info);
-			} else if (radio_courier.isSelected()){
-				user_specific_info.add(courier_specific_info);
-			} else if (radio_restaurant.isSelected()){
-				user_specific_info.add(restaurant_specific_info);
+			
+			username = username_JTF.getText();
+			passwort = password_JTF.getText();
+			passwortConf = passwordConf_JTF.getText();
+			
+			if(checkIfPassWordIsEqual(passwort,passwortConf)){
+					
+				user_specific_info.removeAll();
+				
+				surname_JTF.setText("Insert your surname");
+				phoneNum_JTF.setText("Insert your phone number");
+				emailAddress_JTF.setText("Insert your Email address");
+				xCoordinate_JTF.setText("Insert xCoordinate");
+				yCoordinate_JTF.setText("Insert yCoordinate");
+				
+				if (radio_customer.isSelected()){
+					
+					customer_specific_info.add(surname_JTF,BorderLayout.NORTH);
+					customer_specific_info.add(phoneNum_JTF,BorderLayout.WEST);
+					customer_specific_info.add(emailAddress_JTF,BorderLayout.EAST);
+					customer_specific_info.add(address_panel, BorderLayout.SOUTH);
+					
+					user_specific_info.add(customer_specific_info);
+					
+				} else if (radio_courier.isSelected()){
+	
+					courier_specific_info.add(surname_JTF,BorderLayout.EAST);
+					courier_specific_info.add(address_panel, BorderLayout.CENTER);
+					courier_specific_info.add(phoneNum_JTF,BorderLayout.WEST);
+					courier_specific_info.add(address_panel, BorderLayout.SOUTH);
+					
+					user_specific_info.add(courier_specific_info);
+		
+				} else if (radio_restaurant.isSelected()){
+					
+					user_specific_info.add(restaurant_specific_info);
+					
+					restaurant_specific_info.add(address_panel, BorderLayout.SOUTH);
+					
+				}
+				user_global_info.add(home_button, BorderLayout.SOUTH);
+				user_global_info.add(backToRegister_button, BorderLayout.SOUTH);
+				user_global_info.add(register_button, BorderLayout.SOUTH);
+				
+				setCurrentPanel(user_global_info);
+				}
+			else {
+				//TODO pop up window
+				System.out.println("Wrong password!!!");
 			}
-			user_global_info.add(home_button, BorderLayout.SOUTH);
-			user_global_info.add(backToRegister_button, BorderLayout.SOUTH);
-			setCurrentPanel(user_global_info);
-		}
+			}
 	}
 	
 	private class GoToLoginButton implements ActionListener {
 		public void actionPerformed(ActionEvent e){
-			login_panel.add(home_button, BorderLayout.SOUTH);
-			login_panel.add(logIn_button, BorderLayout.SOUTH);
-			setCurrentPanel(login_panel);
+			goToLogInPanel();
 		}
 	}
 	
 	private class GoToRegisterButton implements ActionListener {
 		public void actionPerformed(ActionEvent e){
-			register_panel_info.add(home_button, BorderLayout.SOUTH);
-			register_panel_info.add(createAccount_button, BorderLayout.SOUTH);
-			setCurrentPanel(register_panel_info);
+			goToRegisterPanel();
 		}
 	}
 	
@@ -355,11 +425,102 @@ public class GUIStartFrame {
 			setCurrentPanel(welcome_panel);
 		}
 	}
+	
+	private class RegisterButton implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			
+			xCoordinate = Integer.parseInt(xCoordinate_JTF.getText());
+			yCoordinate = Integer.parseInt(yCoordinate_JTF.getText());
+			name = name_JTF.getText();
+			
+			Address address = new Address(xCoordinate, yCoordinate);
+			
+			if(customer_specific_info.isShowing()){
+				surname = surname_JTF.getText();
+				emailAddress = emailAddress_JTF.getText();
+				phoneNum = phoneNum_JTF.getText();
+				
+				Customer customer = new Customer(name, surname, address, phoneNum, emailAddress, username);
+				try {
+					core.register(customer);
+				} catch (AlreadyUsedUsernameException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				System.out.println("Customer");
+				
+			}
+			else if(courier_specific_info.isShowing()){
+				surname = surname_JTF.getText();
+				phoneNum = phoneNum_JTF.getText();
+				
+				Courier courier = new Courier(name, surname, address, phoneNum, username);
+				
+				try {
+					core.register(courier);
+				} catch (AlreadyUsedUsernameException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				System.out.println("Courier");
+			}
+			else if(restaurant_specific_info.isShowing()){
+				
+				Restaurant restaurant = new Restaurant(name, address, username);
+				
+				try {
+					core.register(restaurant);
+				} catch (AlreadyUsedUsernameException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				System.out.println("Restaurant");
+			}
+			passwordUserMap.put(username, passwort);
+			goToLogInPanel();
+		}
+	
+	}
+	
+	private class LoginButton implements ActionListener {
+		public void actionPerformed(ActionEvent e){
+			username = username_JTF.getText();
+			String code = password_JTF.getText();
+			
+			if(!passwordUserMap.containsKey(username)){
+				//TODO PopUp
+				goToRegisterPanel();
+			}
+			else if(!passwordUserMap.get(username).equals(code)){
+				System.out.println("Wrong Password !!!");
+				//TODO popUp
+			}
+			else{
+				System.out.println(core.logIn(username));
+				User current_user = core.getCurrent_user();
+				System.out.println(current_user.getName());
+				
+				if (current_user instanceof Courier){
+					//TODO new Function 
+				} else if (current_user instanceof Customer){
+					//TODO new Function 
+				} else if (current_user instanceof Manager){
+					//TODO new Function
+				} else if (current_user instanceof Restaurant){
+					//TODO new Function
+				}
+			}
+		}
+	}
 
 	
 	/*********************************************************/
 	/* Launch */
 	public static void main(String[] args) {
+		
 		GUIStartFrame gui = GUIStartFrame.getInstance();
 		gui.open(0, 0, 600, 400);
 	}
