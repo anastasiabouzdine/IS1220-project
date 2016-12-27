b0e3138b2d51add2ab89d698e88f0136ce604f3a
John de Wasseige <johndewasseige@me.com>
2016-12-28 00:49:37 +0100

    updated eval files and smarter eval tester

---
+++ code/tests/CLUIOneCommandsTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package tests;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import clui.Command;
+import clui.CommandLine;
+import parsers.ParseCommands;
+
+/**
+ * @author john
+ *
+ */
+public class CLUIOneCommandsTest {
+
+
+	public static void main(String[] args) {
+		CommandLine CL = CommandLine.getInstance();
+		ArrayList<Command> command_list = ParseCommands.parseCommands("src/txtFILES/mf_commands.txt");
+		CL.setCommand_list(command_list);
+
+		String folder_path = "./eval/oneCommand/";
+		File folder = new File(folder_path);
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			CL.launchFromFile(folder_path + listOfFiles[i].getName());
+		}
+
+	}
+
+}
